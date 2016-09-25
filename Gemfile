fbe5806ba3d041361a013a717a150c590f730a2e
skitn <tech.ntaka@gmail.com>
2016-09-25 15:26:20 +0000

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'bcrypt-ruby', '3.1.5'
+gem 'faker', '1.6.6'
+gem 'will_paginate', '3.1.3'
+gem 'bootstrap-will_paginate', '0.0.10'
 
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3'
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923162210) do
+ActiveRecord::Schema.define(version: 20160925150037) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
